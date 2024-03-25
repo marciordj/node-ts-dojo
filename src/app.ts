bfb0f72907e4cee006f933232a9423167499ef16
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2024-03-25 19:28:22 -0300

    restart

--- src/app.ts
+++
@@ -1,6 +0,0 @@
-import { appRoutes } from './http/routes'
-import fastify from 'fastify'
-
-export const app = fastify()
-
-app.register(appRoutes)
--- src/http/routes.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyInstance } from 'fastify'
-import { register } from './controller/register.controller'
-
-export const appRoutes = async (app: FastifyInstance) => {
-  app.post('/users', register)
-}
